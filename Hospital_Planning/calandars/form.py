8c4eacf8f40559304d271b5f55dea61174d4aada
kadoret <kdo.nguyen@gmail.com>
2012-12-24 17:34:11 +0100

    remove unused files

--- Hospital_Planning/calandars/form.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from models import Calandars
-
-class CalandarsForm(forms.ModelForm):
-    class Meta:
-        model =  Calandars
--- Hospital_Planning/users/views.py
+++
@@ -1,8 +0,0 @@
-#-*- coding: utf-8 -*-
-from django.http import HttpResponse
-from django.shortcuts import render
-from users.models import Users
-
-def index(request):
-	anUserList = Users.objects.all()
-	return render(request, 'users/index.html', {'userList': anUserList})
